220ac1bcf574eba81b23f7ef1e376203595aae10
rafaelmendesf <rafael.mendes14207@gmail.com>
2025-03-25 22:01:51 -0300

    commit

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,3 +1,9 @@
 function calcular(){
-    alert("Funcionou")
+    // Vamos criar 2 variáveis
+    // JS as variáveis não possuem tipo
+    let mascote, homenagem
+    mascote = document.getElementById("mascote").value
+    alert(mascote)
+    homenagem = document.getElementById("homenagem").value
+    alert(homenagem)
 }
