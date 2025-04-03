329f4bf490579e1997034ec4bb92d04648b39054
rafaelmendesf <rafael.mendes14207@gmail.com>
2025-04-03 20:20:34 -0300

    commit

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -17,18 +17,20 @@ function calcular(){
 
     equipe = document.getElementById("equipe").value
 
-    if (kit >= 97) {
-        soma = soma + 5000 + ((kit - 97) * 30)
-    }
-    else if (kit >= 78) {
-        soma = soma + 4000 + ((kit - 78) * 30)
-    }
-    else if (kit >= 49) {
-        soma = soma + 2500 + ((kit - 49) * 30)
-    }
-    else if (kit >= 19) {
-        soma = soma + 1000 + ((kit - 19) * 30)
-    }
+    if(equipe == "Laranja"){
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+    }   
 
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
 
