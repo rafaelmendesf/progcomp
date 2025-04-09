9fc64ff47665c3354afc920813433ec4b64fbac3
rafaelmendesf <rafael.mendes14207@gmail.com>
2025-04-08 22:06:20 -0300

    commit

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,7 +1,7 @@
 function calcular(){
     // Vamos criar 2 variáveis
     // JS as variáveis não possuem tipo
-    let mascote, homenagem, leite, kit, suplemento, soma, equipe
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe, sangue 
 
     mascote = Number(document.getElementById("mascote").value)
 
@@ -13,11 +13,14 @@ function calcular(){
 
     suplemento = Number(document.getElementById("suplemento").value)
 
+    sangue = Number(document.getElementById("sangue").value)
+
     soma = (mascote) + (homenagem) + (2 * (leite)) 
 
     equipe = document.getElementById("equipe").value
 
     if(equipe == "Laranja"){
+
         if (kit >= 97 && suplemento >= 49) {
             soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
         }
@@ -30,20 +33,87 @@ function calcular(){
         else if (kit >= 19 && suplemento >= 10) {
             soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
         }
+        // verificar sangue
+        if (sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
+        
     }
     else if(equipe == "Preta"){
+         
+        if (kit >= 97 && suplemento >= 49) { // alterar valores (metas)
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+        // verificar sangue
+        if (sangue >= 52){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
 
     }
     else if(equipe == "Roxa"){
 
+        if (kit >= 97 && suplemento >= 49) { // alterar valores (metas)
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+        // verificar sangue
+        if (sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
+
     }
     else if(equipe == "Verde"){
+
+        if (kit >= 97 && suplemento >= 49) { // alterar valores (metas)
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+        }
+        else if (kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+        }
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+        // verificar sangue
+        if (sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else {
+            soma = soma + (sangue * 20)
+        }
 
     }
     else { // equipe vermelha
     
     }
-
     
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
 
